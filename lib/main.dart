f9dcafc5044d16811d93d2f9e58d3adfa218dd44
omarhelmy2004 <omarhelmy2004@hotmail.com>
2024-07-31 00:19:35 +0300

    initializing app

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SavingApp());
+}
+
+class SavingApp extends StatelessWidget {
+  const SavingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
