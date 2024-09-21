8b89aa98f693690cec0ac573d8931069b41c1360
omarhelmy2004 <omarhelmy2004@hotmail.com>
2024-09-21 04:55:26 +0300

    Colors pallets for pie chart

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+
 var greyColor = Colors.grey[800];
 var mainChartsColor = Colors.grey[800]!.withOpacity(1);
+
 const List<Color> pieChartColors = [
-  Color(0xFF4FC3F7), // Light Blue
-  Color(0xFFFF7043), // Orange
-  Color(0xFFAB47BC), // Purple
-  Color(0xFF66BB6A), // Green
-  Color(0xFFFFEB3B), // Yellow
-  Color(0xFFEF5350), // Red
-  Color(0xFF26A69A), // Teal
-  Color(0xFFEC407A), // Pink
-  Color(0xFFCDDC39), // Lime Green
-  Color(0xFF7E57C2), // Deep Purple
+  Color(0xFF4A4A4A), // Dark Grey (Background-like)
+  Color(0xFF707070), // Muted Grey (Accent)
+  Color(0xFF52B788), // Muted Green (Rates Section Color)
+  Color(0xFF9C27B0), // Muted Purple (Header/Icons)
+  Color(0xFFE0E0E0), // Light Grey (Text Color Contrast)
+  Color(0xFFF44336), // Muted Red (Error/Alert)
+  Color(0xFF795548), // Brownish Grey (Neutral Element)
+  Color(0xFF64B5F6), // Soft Blue (Icons/Buttons)
+  Color(0xFFFFC107), // Soft Yellow (Highlight/Attention)
+  Color(0xFF757575), // Muted Darker Grey (Subtle Element)
 ];
