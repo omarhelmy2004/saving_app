57d120ef7ae773bf4cfc06e362d5dd898ca7c9ef
omarhelmy2004 <omarhelmy2004@hotmail.com>
2024-10-02 06:50:44 +0300

    budget model

---
+++ lib/models/budget_model.dart
@@ -0,0 +1,6 @@
+class TransactionsModel {
+  String title;
+  double budgetedPrice;
+  double spentPrice;
+  TransactionsModel({required this.title, required this.budgetedPrice, required this.spentPrice});
+}
