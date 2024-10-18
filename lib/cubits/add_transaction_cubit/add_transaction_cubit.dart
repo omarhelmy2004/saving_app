a79b5e8d03701ab7d50256a3cfa3163af6d435cd
omarhelmy2004 <omarhelmy2004@hotmail.com>
2024-10-18 19:25:53 +0300

    add method in transactions

--- lib/cubits/add_transaction_cubit/add_transaction_cubit.dart
+++ lib/cubits/add_transaction_cubit/add_transaction_cubit.dart
@@ -1,12 +1,43 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
+import 'package:saving_app/constants/strings.dart';
+import 'package:saving_app/models/income_transaction_model.dart';
 import 'package:saving_app/models/outcome_transaction_model.dart';
 
 part 'add_transaction_state.dart';
 
 class AddTransactionCubit extends Cubit<AddTransactionState> {
   AddTransactionCubit() : super(AddTransactionInitial());
-  addOutComeTransaction(OutcomeModel outcomeModel){
-    
+
+  Future<void> addOutComeTransaction(OutcomeModel outcomeModel) async {
+    try {
+      // Open the Hive box
+      var transactionsBox = Hive.box(kTransactionsBox);
+
+      // Add the transaction to the box
+      await transactionsBox.add(outcomeModel);
+
+      // Emit success state
+      emit(AddTransactionSuccess());
+    } catch (e) {
+      // Emit error state if something goes wrong
+      emit(AddTransactionFailure("Failed to add transaction: ${e.toString()}"));
+    }
+  }
+  Future<void> addInComeTransaction(IncomeModel incomeModel) async {
+    try {
+      // Open the Hive box
+      var transactionsBox = Hive.box(kTransactionsBox);
+
+      // Add the transaction to the box
+      await transactionsBox.add(incomeModel);
+
+      // Emit success state
+      emit(AddTransactionSuccess());
+    } catch (e) {
+      // Emit error state if something goes wrong
+      emit(AddTransactionFailure("Failed to add transaction: ${e.toString()}"));
+    }
   }
 }
